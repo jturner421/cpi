09a7b8f2636f647c2d48f170edcfe1c8408c11b5
Joel Turner <joel_turner@wiwd.uscourts.gov>
2023-04-07 14:49:01 -0500

    Wip - Addressing dismissals that conflict with UA dates

--- ua_dates.py
+++ ua_dates.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 from dataclasses import dataclass, field
+from enum import Enum
 import dill
 import pandas as pd
-from colorama import Fore, Style
+from colorama import Fore
 
+
+class DismissalType(Enum):
+    voldism = 'Voluntary Dismissal'
+    termpscs = 'No Trust Fund Statement'
+    termcs = 'Civil Case Terminated'
 
 @dataclass
 class CaseDates:
@@ -21,6 +27,7 @@ class CaseDates:
     ltp_date: datetime = None
     initial_pretrial_conference_date: datetime = None
     dismissal_date: datetime = None
+    dismissal_reason: str = None
     amended_complaints: list = field(default_factory=list)
     order_to_leave_dates: list = field(default_factory=list)
     case_reopen_dates: list = field(default_factory=list)
@@ -29,16 +36,25 @@ class CaseDates:
     dismissal_dates: list = field(default_factory=list)
 
     def dict(self):
-        if len(self.order_to_leave_dates) == 2 and \
-                self.order_to_leave_dates[0]['seqno'] == self.order_to_leave_dates[1]['seqno']:
+        if len(self.order_to_leave_dates) == 1:
             self.ltp_date = datetime.strptime(self.order_to_leave_dates[0]['ltp_date'], '%Y-%m-%d')
-        else:
+        elif len(self.order_to_leave_dates) > 2:
             print(f' More then one Leave order in {self.caseid}')
+        else:
+            self.ltp_date = None
+            print(f' No Leave order in {self.caseid}')
 
-        if len(self.ua_dates) == 1:
-            self.ua_date = datetime.strptime(self.ua_dates[0]['ua_date'][0][0], '%Y-%m-%d')
+        if len(self.ua_dates):
+            self.ua_date = datetime.strptime(self.ua_dates[0]['ua_date'], '%Y-%m-%d')
+
         if len(self.dismissal_dates):
             self.dismissal_date = datetime.strptime(self.dismissal_dates[-1]['dis_date'], '%Y-%m-%d')
+            self.dismissal_reason = DismissalType[self.dismissal_dates[-1]['dis_reason']].value
+        if self.dismissal_date is not None and self.ua_date is not None:
+            if self.ua_date > self.dismissal_date:
+                print(f'{Fore.RED} UA date after dismissal date in {self.caseid}{Fore.RESET}')
+                self.ua_date = None
+
         if self.initial_pretrial_conference_date:
             self.initial_pretrial_conference_date = datetime.strptime(self.initial_pretrial_conference_date,
                                                                       '%Y-%m-%d')
@@ -59,13 +75,15 @@ class CaseDates:
         del _dict['complaint_docnum']
         del _dict['screening_docnum']
         del _dict['ifp_docnum']
+        del _dict['case_reopen_dates']
         return _dict
 
 
 def find_ua_date(cmp_docnum, screening_docnum, ua):
+    ua['dt_text'] = ua['dt_text'].str.lower()
     ua_dates = []
-    matches = [cmp_docnum, str(screening_docnum), "Screening", 'Complaint', 'forma pauperis', 'Habeas', 'Reopen',
-               'Social Security', 'Bankruptcy']
+    matches = [cmp_docnum, str(screening_docnum), "screening", 'complaint', 'pauperis', 'habeas', 'reopen',
+               'social security', 'bankruptcy', 'prepayment']
     for index, row in ua.iterrows():
         a_string = row['dt_text']
         if any(x in a_string for x in matches):
@@ -75,7 +93,7 @@ def find_ua_date(cmp_docnum, screening_docnum, ua):
     return ua_dates
 
 
-def _find_complaint(case_dates, target):
+def _find_complaint(target):
     case = CaseDates(caseid=target['de_caseid'].iloc[0])
     cmp = target.query('dp_type == "motion" and dp_sub_type == "2255" or dp_sub_type=="cmp" or dp_sub_type=="pwrithc" '
                        'or dp_sub_type == "ntcrem" or dp_sub_type == "emerinj" or dp_sub_type == "bkntc" '
@@ -130,24 +148,18 @@ def _get_ua_date(case_dates, ua):
     if not ua.empty:
         ua_dates = find_ua_date(case_dates.complaint_docnum, case_dates.screening_docnum, ua)
         if ua_dates:
-            case_dates.ua_dates.append({'ua_date': ua_dates})
-    # else:
-    #     pass
-    # ua_date = np.nan
-    # case_dates.ua_dates.append({'ua_date': ua_date})
+            for ua_date in ua_dates:
+                case_dates.ua_dates.append({'ua_date': ua_date[0], 'ua_text': ua_date[1]})
     return case_dates
 
 
 def _check_for_trust_fund_statement(case_dates, target):
-    # Was the case dimissed due to lack or prisoner trust fund statement?
+    # Was the case dismissed due to lack or prisoner trust fund statement?
     mask = target['dp_sub_type'] == 'termpscs'
     term = target[mask]
     if not term.empty:
         term_date = term['de_date_filed'].iloc[0]
         case_dates.trust_fund_dates.append(term_date)
-    # else:
-    #     term_date = np.nan
-    #     case_dates.trust_fund_dates.append(term_date)
     return case_dates
 
 
@@ -160,7 +172,8 @@ def _early_dismissal(case_dates, target):
     if not dism.empty:
         dism.sort_values(by=['de_date_filed'], inplace=True, ascending=True)
         dis_date = dism['de_date_filed'].iloc[-1]
-        case_dates.dismissal_dates.append({'dis_date': dis_date})
+        dis_reason = dism['dp_sub_type'].iloc[-1]
+        case_dates.dismissal_dates.append({'dis_date': dis_date, 'dis_reason': dis_reason})
     return case_dates
 
 
@@ -177,11 +190,13 @@ def _get_reopen_date(case_dates, target):
 def _get_leave_to_proceed(case_dates, target):
     ltp = target.loc[target['dp_sub_type'] == 'leave']
     if not ltp.empty:
+        ltp = ltp.drop_duplicates(subset=['dp_seqno'])
         for index, row in ltp.iterrows():
             case_dates.order_to_leave_dates.append({'ltp_date': row['de_date_filed'], 'seqno': row['dp_seqno']})
     else:
         case_dates = _early_dismissal(case_dates, target)
     return case_dates
+
 
 def _get_pretrial_conference_date(case_dates, target):
     # Check for pretrial conference
@@ -190,6 +205,8 @@ def _get_pretrial_conference_date(case_dates, target):
         for index, row in pretrial.iterrows():
             case_dates.initial_pretrial_conference_date = row['de_date_filed']
     return case_dates
+
+
 def get_case_milestone_dates(case_id: int, target: pd.DataFrame, case_type: str) -> pd.DataFrame:
     """
     """
@@ -197,7 +214,7 @@ def get_case_milestone_dates(case_id: int, target: pd.DataFrame, case_type: str)
     print(Fore.BLUE + f'Getting ua dates for {case_id}...', flush=True)
     case_dates.append({'case_id': case_id})
     # locate complaints
-    case_dates = _find_complaint(case_dates, target)
+    case_dates = _find_complaint(target)
     case_dates.case_type = case_type
     if case_dates.complaint_date or case_dates.screening_date:
         # check for ifp motion
@@ -209,11 +226,8 @@ def get_case_milestone_dates(case_id: int, target: pd.DataFrame, case_type: str)
         case_dates = _get_ua_date(case_dates, ua)
         if not case_dates.ua_dates:
             case_dates = _check_for_trust_fund_statement(case_dates, target)
-
-    if ua.empty:
-        # Was there a voluntary dismissal or termination due to no prisoner trust fund statement?
-        case_dates = _early_dismissal(case_dates, target)
-
+    # Was there a voluntary dismissal or termination due to no prisoner trust fund statement?
+    case_dates = _early_dismissal(case_dates, target)
     # check for leave to proceed
     case_dates = _get_leave_to_proceed(case_dates, target)
     case_dates = _get_pretrial_conference_date(case_dates, target)
@@ -253,9 +267,6 @@ def main():
         ua_dates.append(get_case_milestone_dates(caseid, target, case_type))
     with open('ua_dates.pkl', 'wb') as f:
         dill.dump(ua_dates, f)
-
-    # with open('ua_dates.pkl', 'rb') as f:
-    #     ua_dates = dill.load(f)
 
 
 if __name__ == '__main__':
